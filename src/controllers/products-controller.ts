ab4f4d67ffb995fa664dc658a9762ea1c3fc6270
CaioLima <caiohenrys50@gmail.com>
2026-07-19 20:23:52 -0300

    create product

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,5 +1,6 @@
-import { AppError } from "@/utils/app-error";
+import { knex } from "@/database/knex";
 import { Request, Response, NextFunction } from "express";
+import z from "zod";
 
 
 class ProductsController {
@@ -8,6 +9,24 @@ class ProductsController {
       return response.json({ message: "OK" })
 
     }catch(error){
+      next(error)
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0)
+      })
+
+      const { name, price } =  bodySchema.parse(request.body)
+
+      await knex<ProductRepository>("products").insert({name, price})
+
+      return response.status(201).json()
+    } catch (error) {
       next(error)
     }
   }
