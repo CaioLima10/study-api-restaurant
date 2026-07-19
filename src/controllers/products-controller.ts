594f8bd2745aa8dfc406f1b83135c1d188d1fd78
CaioLima <caiohenrys50@gmail.com>
2026-07-19 05:40:35 -0300

    separator function routes and controllers

---
+++ src/controllers/products-controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+
+
+class ProductsController {
+  async index(request: Request, response: Response, next: NextFunction) {
+    try{
+      return response.json({ message: "OK" })
+    }catch(error){
+      next(error)
+    }
+  }
+}
+
+export { ProductsController }
