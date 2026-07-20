609e5db0781d37769632c402a123c93f09973a1e
CaioLima <caiohenrys50@gmail.com>
2026-07-19 21:13:50 -0300

    update

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -37,6 +37,30 @@ class ProductsController {
       next(error)
     }
   }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0)
+      })
+      
+      const bodySchemaParams = z.object({
+        id: z.string(),
+      })
+
+      const { name, price } = bodySchema.parse(request.body)
+      const { id } = bodySchemaParams.parse(request.params)
+
+      await knex<ProductRepository>("products").update({ name, price }).where({ id: id })
+
+      return response.status(200).json()
+
+    } catch (error) {
+      next(error)
+    }
+  }
+
 }
 
 export { ProductsController }
